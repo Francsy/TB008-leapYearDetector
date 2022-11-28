399208a1306f8337dedee7e2545adb999d26cfd4
Francsy <francsyludens@gmail.com>
2022-11-29 00:52:27 +0100

    Exercise solved and exploring another ways

---
+++ scripts/leapyeardetector.js
@@ -0,0 +1,88 @@
+//Hacemos que la variable year sea designada a través del prompt
+
+let year = prompt("Introduce tu año favorito");
+
+//El primer if es para filtrar que sea un número:
+
+if (year >= 0 || year <= 0) {
+    if ((year % 4 == 0 && year % 100 != 0) || (year % 4 == 0 && year % 400 == 0)) {
+        console.log(`Puedes estar seguro: el año ${year} es bisiest`)
+    } else {
+        console.log(`El ${year} es un año normalísimo`)
+    }
+} else { console.log(`${year} no es un año`); }
+
+/* En el primer if ponemos una disyunción de dos proposiciones:
+- Que sea múltiplo de 4 mientras no sea de 100.
+- Que sea múltiplo de 4 y que también lo sea de 400.
+Si al menos una es verdadera, se imprimirá que es bisiesto
+
+
+
+
+
+
+// Duda: ¿Hay otra forma más sencilla de hacer que solo se metan variables numéricas? */
+
+
+
+
+
+
+
+
+
+
+
+
+
+/* Otra forma:
+
+let year = prompt("Introduce año")
+
+while(year>=0 || year<=0){
+        
+    if ((year%4==0 && year%100!=0) || (year%4==0 && year%400==0)) {
+        console.log("Bisiesto");
+        break;
+    }
+    
+    if (year) {
+        console.log("no bisiesto")
+        break;
+    }
+            
+}
+ */
+
+
+
+/* Otra forma:
+
+if (year >= 0 || year <= 0) {
+    switch (year) {
+        case ((year%4==0 && year%100!=0) || (year%4==0 && year%400==0)):
+            console.log("Puedes estar seguro: es bisiesto");
+            break;
+        default:
+            console.log("Es un año normalísimo");
+            break;
+    }
+} else {
+    console.log("Eso no es un año");
+} 
+*/
+
+
+
+
+/* Última opción vista
+
+function Bisiesto(year) {
+    return year%100===0 ? year%400===0 : year%4=== 0;
+}
+if (Bisiesto(year)===true) {
+    console.log("El año " + year + " es bisiesto");
+} else {console.log("El año " + year + " es normalísimo");
+} */
+
